094f8e79b10306debeea4fdaadd648ea5f68ceb4
Devander Kumra <devander.kumra216@gmail.com>
2023-06-07 15:06:16 +0530

    oops

--- Cpp programs/Programs/File Handeling/readingWriting[728].cpp
+++ Cpp programs/Programs/File Handeling/readingWriting[728].cpp
@@ -3,26 +3,24 @@
 using namespace std;
 int main(){
   string name;
-//   fstream f("demo.txt",ios::in);
-// //   cout<<"Enter you name";
-// //   cin>>name;
-//  while(getline(f,name))
-//  {
-//     cout<<"name ="<<name<<endl;
-//  }
-//   f.close();
-
-//   ofstream outStr("demo2.txt");
-//   outStr<<"Hello World";
-//   outStr.close();
-
-  ifstream instr("demo2.txt");
-  while(getline(instr,name))
+  fstream f("demo.txt",ios::in);
+//   cout<<"Enter you name";
+//   cin>>name;
+ while(getline(f,name))
  {
     cout<<"name ="<<name<<endl;
  }
-  instr.close();
+  f.close();
 
+  ofstream outStr("demo2.txt");
+  outStr<<"Hello World";
+  outStr.close();
 
+//   ifstream instr("demo2.txt");
+//   while(getline(instr,name))
+//  {
+//     cout<<"name ="<<name<<endl;
+//  }
+//   instr.close();
 
 }
